921eefd780f72dceb0363a764f5b64b09ab19ae6
Erhan <demirci.erhan@windowslive.com>
2022-10-02 13:18:25 +0300

    Initial Commit

---
+++ swiftUiHostingtoUIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftUiHostingtoUIkit
+//
+//  Created by erhan demirci on 2.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
